ff2b8ada20d8a43bd85b2e5245e5f7f11c60b48c
Murdism <muradsmebrahtu@gmail.com>
2024-11-12 19:45:03 +0400

    added_rosbag_reply

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ This package contains ROS2 wrappers for iniVation cameras. The package is based 
     <img src="images/event_camera_images.gif" width=1000>
 </p>
 
-## Installation
+<!-- ## Installation -->
 
-Tested on Ubuntu 22.04 with ROS2 Humble.
-First, clone the repository.
+
+<!-- First, clone the repository.
     
 ```
 git clone https://github.com/Telios/dv-ros2.git
@@ -21,35 +21,76 @@ cd docker && docker compose up
 xhost +local:docker
 docker compose up
 ```
-Otherwise, follow the instructions below.
+Otherwise, follow the instructions below. -->
 
 ### Dependencies
 Install dependencies using the following commands:
 
-```
+```bash
 sudo add-apt-repository ppa:inivation-ppa/inivation
 sudo apt-get update
 sudo apt-get install dv-processing cmake boost-inivation libopencv-dev libeigen3-dev libcaer-dev libfmt-dev liblz4-dev libzstd-dev libssl-dev
+sudo apt-get install dv-runtime-dev
 ```
 
-### Build
+###  Installation
+Tested on Ubuntu 22.04 with ROS2 Humble.
 
-Build the package using the following commands:
-
+- Prepare workspace  
+```bash
+mkdir ~/event_ws
+cd ~/event_ws
+mkdir src
+cd src
 ```
+- clone repo
+```bash
+git clone https://github.com/Murdism/dv-ros2.git
+```
+Source ROS2:
+```bash
 source /opt/ros/humble/setup.bash
-colcon build
 ```
+- Build and source:
+
+```bash
+colcon build
+source install/setup.bash
+```
+
 
 ### Run
 
-To test the camera driver, run the following commands:
-
+- To only capture and publish events at /events topic use the following command:  
+```bash
+ros2 run dv_ros2_capture dv_ros2_capture_node 
 ```
-source install/setup.bash
+- To capture, publish and vizualize events use the following command:
+```bash
 ros2 launch dv_ros2_visualization all.launch.py
 ```
-A GUI should open with the 4 different camera streams.
+This should open a GUI with the 4 different camera streams.
+ 
+
+### Record Events
+- To record ros2 bag of events use the following command:
+```bash
+ros2 bag record -o <output_directory> <topic_names>
+```
+```bash
+ ros2 bag record -o events_rosbag /events
+```
+
+#### Run from Rosbag (Replay Events)
+Replay recorded events:
+```bash
+ros2 bag play /path/to/rosbag 
+```
+To visualize results launch viz_rosbag
+
+```bash
+ros2 launch dv_ros2_visualization viz_rosbag.py 
+```
 
 ### Repository structure
 
---
+++ dv_ros2_visualization/launch/viz_rosbag.py
@@ -0,0 +1,101 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os, yaml
+
+def generate_launch_description():
+    acc_package_name = 'dv_ros2_accumulation'
+    acc_config_path = os.path.join(get_package_share_directory(acc_package_name), 'config', 'config.yaml')
+
+    acc_node_name = f'{acc_package_name}_node'
+    with open(acc_config_path, 'r') as file:
+        acc_config = yaml.safe_load(file)[acc_package_name]['ros__parameters']
+    
+    cap_package_name = 'dv_ros2_capture'
+    cap_config_path = os.path.join(get_package_share_directory(cap_package_name), 'config', 'config.yaml')
+
+    cap_node_name = f'{cap_package_name}_node'
+    with open(cap_config_path, 'r') as file:
+        cap_config = yaml.safe_load(file)[cap_package_name]['ros__parameters']
+
+    vis_package_name = 'dv_ros2_visualization'
+    vis_config_path = os.path.join(get_package_share_directory(vis_package_name), 'config', 'config.yaml')
+
+    vis_node_name = f'{vis_package_name}_node'
+    with open(vis_config_path, 'r') as file:
+        vis_config = yaml.safe_load(file)[vis_package_name]['ros__parameters']
+
+    track_package_name = 'dv_ros2_tracker'
+    track_config_path = os.path.join(get_package_share_directory(track_package_name), 'config', 'config.yaml')
+
+    track_node_name = f'{track_package_name}_node'
+    with open(track_config_path, 'r') as file:
+        track_config = yaml.safe_load(file)[track_package_name]['ros__parameters']
+    
+    return LaunchDescription([
+        # Node(
+        #     package=cap_package_name,
+        #     executable=cap_node_name,
+        #     name=cap_node_name,
+        #     parameters=[cap_config],
+        #     output='screen',
+        #     emulate_tty=True,
+        # ),
+        Node(
+            package=acc_package_name,
+            executable=acc_node_name,
+            name=f'{acc_node_name}_frame',
+            namespace=f'{acc_node_name}_frame',
+            parameters=[acc_config],
+            output='screen',
+            emulate_tty=True,
+            remappings=[
+                (f'/{acc_node_name}_frame/events', '/events'),            
+            ]
+        ),  
+        Node(
+            package=acc_package_name,
+            executable=acc_node_name,
+            name=f'{acc_node_name}_edge',
+            namespace=f'{acc_node_name}_edge',
+            parameters=[acc_config | {'accumulation_mode': "EDGE", 'event_contribution': 0.3}],
+            output='screen',
+            emulate_tty=True,
+            remappings=[
+                (f'/{acc_node_name}_edge/events', '/events'),            
+            ]
+        ),
+        Node(
+            package=vis_package_name,
+            executable=vis_node_name,
+            name=vis_node_name,
+            namespace=vis_node_name,
+            parameters=[vis_config],
+            output='screen',
+            emulate_tty=True,
+            remappings=[
+                (f'/{vis_node_name}/events', '/events'),            
+            ]
+        ),
+        Node(
+            package=track_package_name,
+            executable=track_node_name,
+            name=track_node_name,
+            namespace=track_node_name,
+            parameters=[track_config],
+            output='screen',
+            remappings=[
+                (f'/{track_node_name}/events', '/events'),
+                (f'/{track_node_name}/camera_info', '/camera_info'),            
+            ],
+            emulate_tty=True,
+        ),
+        Node(
+            package='rqt_gui',
+            executable='rqt_gui',
+            name='rqt_gui',
+            output='screen',
+            arguments=['--perspective-file', os.path.join(get_package_share_directory('dv_ros2_visualization'), 'config', 'all.perspective')],
+            emulate_tty=True,
+        )
+    ])
